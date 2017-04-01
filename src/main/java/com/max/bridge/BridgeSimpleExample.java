a24cc3dda0dc22dd6fa60f63fa02fe0a7b457e59
Max <maxrjohnson23@gmail.com>
2017-04-01 10:41:58 -0500

    Add simple Bridge pattern example

--- src/main/java/com/max/bridge/BridgeSimpleExample.java
+++ src/main/java/com/max/bridge/BridgeSimpleExample.java
@@ -1,7 +1,9 @@
 package com.max.bridge;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Simple example demonstrating the bridge pattern using a JDBC driver
@@ -13,6 +15,20 @@ public class BridgeSimpleExample {
         try {
             // JDBC is an API
             DriverManager.registerDriver(new org.apache.derby.jdbc.EmbeddedDriver());
+
+            String dbUrl = "jdbc:derby:memory:codejava/webdb;create=true";
+
+            Connection conn = DriverManager.getConnection(dbUrl);
+
+            Statement st = conn.createStatement();
+
+            // This client is an abstraction and can work with any database that has a driver
+            st.executeUpdate("CREATE TABLE Address (ID INT, StreetName VARCHAR(20), City VARCHAR(20))");
+
+            System.out.println("Table created");
+
+            st.close();
+            conn.close();
 
         } catch (SQLException e) {
             e.printStackTrace();
